0b41b5caf42545e1458b9ef426e2a9a12395569b
Baranuel <samuel.baran98@gmail.com>
2023-03-06 15:39:23 +0100

    getting data from db

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,9 @@
+import clientPromise from "../../../lib/mongodb";
+
 export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
+  const client = await clientPromise;
+  const db = client.db("portfolio-samuel").collection("projects");
+  const projects = await db.find({}).toArray();
+  console.log(projects);
+  return new Response(JSON.stringify(projects));
 }
