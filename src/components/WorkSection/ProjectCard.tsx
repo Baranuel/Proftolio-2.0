455cadd32898e2cfd0486238485f611fe913359c
Samuel Baran <samuel.baran98@gmail.com>
2023-04-06 16:56:11 +0200

    some styling , can't decide how stuff should look

--- src/components/WorkSection/ProjectCard.tsx
+++ src/components/WorkSection/ProjectCard.tsx
@@ -6,22 +6,16 @@ function ProjectCard() {
         <div
           onMouseOver={() => setActive(true)}
           onMouseLeave={() => setActive(false)}
-          className={`flex flex-col justify-between hover:cursor-pointer p-2 h-[225px] w-[175px] bg-gradient-to-b ${
-            !active ? "from-[#FCFCFC]" : "from-[#F4E8F8]"
-          }  to-white  rounded-2xl outline ${
-            active ? "outline-2 outline-[#B863FF]" : "outline-[#C1B5E3]"
-          } transition-all duration-100           relative
+          className={`flex flex-col justify-between hover:cursor-pointer p-2 h-[225px] w-[175px]  rounded-2xl  ${
+            active ? " outline outline-lightPink " : " bg-white shadow-md "
+          } transition-all duration-100 relative
           `}
         >
           <div className="self-center grow w-3/5 bg-transparent  flex items-center justify-center text-white relative z-50">
-            <h1 className={`z-50 ${!active ? "text-[#C1B5E3]" : "text-[#B863FF]"} font-quicksand text-2xl font-semibold transition-all duration-100 `}>Paluba</h1>
-            <div
-              className={` w-full ${
-                !active ? "bg-[#FCFCFC]" : "bg-[#EDCFFF]"
-              } h-full blur-3xl rounded-full absolute`}
-            ></div>
+            <h1 className={`z-50 ${!active ? "text-[#C1B5E3]" : "text-lightPink"} font-quicksand text-2xl font-semibold transition-all duration-100 `}>Paluba</h1>
+      
           </div>
-          <div className={`self-center h-fit font-k2d mb-4 ${!active ? "text-[#C1B5E3] " : "text-[#B863FF] "} absolute bottom-0 font-light transition-all duration-100 `}>Redesign</div>
+          <div className={`self-center h-fit font-k2d mb-4 text-sm text-orange-400 absolute bottom-0 font-light bg-orange-300/20 p-1 px-2 rounded-md transition-all duration-100 `}>Redesign</div>
         </div>
     );
 }
--- src/components/WorkSection/Work.tsx
+++ src/components/WorkSection/Work.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard";
 
 function Work() {
   return (
-    <div className="min-h-screen w-screen bg-white px-64 2xl:px-24 xl:px-12 lg:px-12 sm:px-6 py-24 gap-8">
+    <div className="min-h-screen w-screen bg-pink-100/10 px-64 2xl:px-24 xl:px-12 lg:px-12 sm:px-6 py-24 gap-8">
       <h1 className="text-5xl py-2 font-bold text-transparent w-fit bg-clip-text bg-gradient-to-r from-lightBlue via-lightPink to-darkPurple self-start">
         Projects
       </h1>
@@ -17,8 +17,39 @@ function Work() {
           <ProjectCard />
           <ProjectCard />
         </div>
-        <div className="w-1/2 bg-white h-auto rounded-xl border border-2-gray-200 ">
-            projects
+        <div className="w-1/2 flex flex-col bg-white h-auto rounded-xl border border-2-gray-200 p-10 ">
+            <div className="flex items-center justify-between w-full">
+            <h1 className="my-6 text-2xl">Title of the project</h1>
+            <h3 className="p-1 font-semibold text-lightPink px-4 ">Type</h3>
+            </div>
+            <div className="flex gap-3">
+                <span>1dsa</span>
+                <span>dad</span>
+                <span>dasd</span>
+                <span>dad</span>
+                <span>dasds</span>
+                <span>dada</span>
+                <span>dasd</span>
+            </div>
+            <hr className="my-3" />
+            <div>
+                <span className="font-bold text-md" >Description: </span>
+                <p className="text-justify">
+                        lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Sed euismod, nisl vel tincidunt lacinia,
+                        nunc nisl aliquam mauris,
+                </p>
+            </div>
+            <div className="my-3">
+                <span className="font-bold text-md" >Goal: </span>
+                <p className="text-justify">
+                        lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Sed euismod, nisl vel tincidunt lacinia,
+                        nunc nisl aliquam mauris,
+                </p>
+            </div>
+            <div className="w-full h-1/3 rounded-md bg-black">img</div>
+
         </div>
       </div>
     </div>
