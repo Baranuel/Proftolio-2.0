f9aa82eaf67c57e3c41472bd6b735ca52163fdb8
Samuel Baran <samuel.baran98@gmail.com>
2023-06-27 16:55:45 +0200

    added height

--- src/components/AboutSection/About.tsx
+++ src/components/AboutSection/About.tsx
@@ -62,7 +62,7 @@ function About() {
   const [selectedCard, setSelectedCard] = React.useState(cards[0]);
 
   return (
-    <div className=" h-screen relative w-screen flex flex-col bg-white px-48 2xl:px-24 xl:px-12 lg:px-12 sm:px-4 py-24  gap-8">
+    <div className=" min-h-fit h-screen relative w-screen flex flex-col bg-white px-48 2xl:px-24 xl:px-12 lg:px-12 sm:px-4 py-24  gap-8">
      
       <h1 className="text-5xl max:text-7xl lg:text-4xl lg:self-center py-2 font-bold text-transparent w-fit bg-clip-text bg-gradient-to-r from-lightBlue via-lightPink to-darkPurple self-start">
         About me
